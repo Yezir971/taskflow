8dceb1bd5478f79b7fdb094d9cffdb24a7338942
Yezir971 <james_ahmedaly@yahoo.com>
2025-11-01 19:10:30 +0100

    feat : add pagination task

--- src/app/features/task/components/table/table.ts
+++ src/app/features/task/components/table/table.ts
@@ -1,13 +1,40 @@
-import { Component, input, Signal } from '@angular/core';
-import { TaskStoreModel } from '../../../../core/service/task-store/task-store-models';
+import {
+  Component,
+  input,
+  Signal,
+  AfterViewInit,
+  ViewChild,
+  effect,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { TaskStoreModel } from '../../../../core/service/task-store/task-store-models';
 
 @Component({
   selector: 'app-table',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, MatPaginatorModule, MatTableModule],
   templateUrl: './table.html',
-  styleUrl: './table.css',
+  styleUrls: ['./table.css'],
 })
-export class Table {
-  datas = input.required<() => TaskStoreModel[]>();
+export class Table implements AfterViewInit {
+  displayedColumns: string[] = ['title', 'priority', 'status', 'date', 'assign'];
+  datas = input.required<TaskStoreModel[]>();
+
+  dataSource = new MatTableDataSource<TaskStoreModel>([]);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor() {
+    // ✅ Reactively update MatTableDataSource when signal changes
+    effect(() => {
+      const tasks = this.datas();
+      this.dataSource.data = tasks;
+    });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
